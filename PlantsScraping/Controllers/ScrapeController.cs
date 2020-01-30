2cbd0e69cb311c0cb51f6a2eda005066183d8b3d
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-01-30 13:19:06 +1300

    finish analyzing 1 plant url until description

--- PlantsScraping/Controllers/ScrapeController.cs
+++ PlantsScraping/Controllers/ScrapeController.cs
@@ -36,9 +36,10 @@ namespace PlantsScraping.Controllers
                 key_values["name"] = main_html.SelectSingleNode("h2").InnerText;
                 HtmlNodeCollection paragraph_div = main_html.SelectNodes("p|div"); 
 
-                Dictionary<string, object> categories = Categories(paragraph_div[0]); 
+                Dictionary<string, object> categories = Categories(paragraph_div[0]);
+                categories["Description"] += DescriptionText(paragraph_div); 
 
-                return null; 
+                return categories; 
             }
 
 
@@ -63,11 +64,48 @@ namespace PlantsScraping.Controllers
             {
                 Dictionary<string, object> categories = new Dictionary<string, object>();
                 categories["Description"] = "";
-                string bad_line = "Thanks to Wikipedia for text and information";
                 string html = category_paragraph.InnerHtml;
-
-                string[] paragraphs = html.Split(new string[] { "<br>" }, StringSplitOptions.RemoveEmptyEntries); 
+                string[] paragraphs = html.Split(new string[] { "<br>" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string paragraph in paragraphs)
+                {
+                    HtmlNode div = CategoriesDiv(paragraph);
+                    try
+                    {
+                        HtmlNode strong = div.SelectSingleNode("strong");
+                        string category = strong.InnerText;
+                        category = category.Substring(0, category.IndexOf(":"));
+                        strong.ParentNode.RemoveChild(strong);
+                        categories[category] = div.InnerText; 
+                    }
+                    catch 
+                    {
+                        categories["Description"] +=div.InnerText.Trim()+"\n"; 
+                    }
+                }
+                categories["Description"] = ((string)categories["Description"]).Trim(); 
                 return categories; 
+            }
+
+            private static HtmlNode CategoriesDiv(string html)
+            {
+                HtmlDocument doc = new HtmlDocument();
+                HtmlNode div = doc.CreateElement("div");
+                div.InnerHtml = html;
+                return div; 
+            }
+
+
+            private static string DescriptionText(HtmlNodeCollection paragraph_div)
+            {
+                string description = "";
+                string bad_line = "Thanks to Wikipedia for text and information";
+                for (int i = 1; i < paragraph_div.Count; i++)
+                {
+                    description += paragraph_div[i].InnerText.Trim() + "\n"; 
+                }
+                description = description.Substring(0, description.IndexOf(bad_line));
+                description = description.Trim();
+                return description; 
             }
 
 
